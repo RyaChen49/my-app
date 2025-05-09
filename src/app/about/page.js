fde94027f61b7a3c1b65020f3f262248d8a80b19
RyanChen <nedefnwk@gmail.com>
2025-05-09 15:46:55 +0800

    新增頁面和組件：about、task 和主選單

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import MainMenu from "../../conpoents/MainMenu.js"
+export default function aboutpage()
+{
+    return (
+        <>
+            <MainMenu/>
+            <h1 className="text-bold">About Page</h1>
+        </>
+            
+    )
+};
